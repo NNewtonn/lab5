df118a3389aa493bcb7bdc7b2ac11562bedfb023
Guillaume Banyuls <guillau20@gmail.com>
2024-01-22 11:41:56 +0100

    octavo

--- testdivision.py
+++ testdivision.py
@@ -5,11 +5,12 @@ class TestSumar(unittest.TestCase):
         self.assertEqual(dividir(4, 2), 2)
         self.assertEqual(dividir(-1, 1), -1)
         self.assertEqual(dividir(-1, -1), 1)
+        #x
         with self.assertRaises(ValueError):
             try:
                 dividir(5, 0)
             except:
                 raise ValueError
-
+            
 if __name__ == '__main__':
     unittest.main()
