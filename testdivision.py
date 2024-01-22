6528cc76c9de3589e380318ff7dac6a5a21771cb
Guillaume Banyuls <guillau20@gmail.com>
2024-01-22 11:25:37 +0100

    quinto

--- testdivision.py
+++ testdivision.py
@@ -4,7 +4,7 @@ class TestSumar(unittest.TestCase):
     def test_restar(self):
         self.assertEqual(dividir(4, 2), 2)
         self.assertEqual(dividir(-1, 1), -1)
-        self.assertEqual(dividir(-1, -1), 0)
+        self.assertEqual(dividir(-1, -1), 1)
         with self.assertRaises(ValueError):
             dividir(5, 0)
 if __name__ == '__main__':
